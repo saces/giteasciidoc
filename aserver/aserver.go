f81ee20071a73d3c56495b4957608e2265e1f4ac
saces <saces@c-base.org>
2020-03-09 22:48:43 +0100

    add timeout

--- aserver/aserver.go
+++ aserver/aserver.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"os/exec"
+	"time"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -18,13 +20,22 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	case "POST":
 		name := r.Header.Get("X-Gitea-Raw")
 		sid := r.Header.Get("X-Session-Token")
-		cmd := exec.Command("asciidoctor", "-r", "/asciidoctor-ext/gitea-include.rb", "-s", "--out-file=-", "-")
+
+		// Create a new context and add a timeout to it
+		ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+		defer cancel() // The cancel should be deferred so resources are cleaned up
+
+		cmd := exec.CommandContext(ctx, "asciidoctor", "-r", "/asciidoctor-ext/gitea-include.rb", "-s", "--out-file=-", "-")
 		cmd.Stdin = r.Body
 		cmd.Env = os.Environ()
 		cmd.Env = append(cmd.Env, fmt.Sprintf("GITEA_PREFIX_RAW=%s", name))
 		cmd.Env = append(cmd.Env, fmt.Sprintf("SESSION_TOKEN=%s", sid))
 
 		out, err := cmd.CombinedOutput()
+		if ctx.Err() == context.DeadlineExceeded {
+			fmt.Fprintf(w, "Command took to long, timed out.")
+			return
+		}
 		if err != nil {
 			fmt.Fprintf(w, "Oops: %s\n", err.Error())
 		}
--- scripts/giteawrapper.sh
+++ scripts/giteawrapper.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 main_function() {
-curl -sS --url http://asciidoctor:8080/ -H "X-Session-Token: $SESSION_TOKEN" -H "X-Gitea-Raw: $GITEA_PREFIX_RAW" --data-binary @-
+curl -sS -m 30 --url http://asciidoctor:8080/ -H "X-Session-Token: $SESSION_TOKEN" -H "X-Gitea-Raw: $GITEA_PREFIX_RAW" --data-binary @-
 }
 
 main_function $@ 2>&1
